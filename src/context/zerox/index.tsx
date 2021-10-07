3b80b9b5a5eaf0b5d2f0ce90b1566d36795dddda
Koushik KM <koushikmohan1996@gmail.com>
2021-10-07 22:42:11 +0530

    Add estimated gas limit

--- src/context/zerox/index.tsx
+++ src/context/zerox/index.tsx
@@ -224,7 +224,7 @@ const ZeroXProvider: FunctionComponent = ({ children }) => {
           exchange.batchFillLimitOrders(orders, signatures, amountsStr, false, {
             value: ethers.utils.parseEther(feeInEth),
             gasPrice: ethers.utils.parseUnits(gasPrice.toString(), 'gwei'),
-            gasLimit: gasLimit.toNumber(),
+            gasLimit: gasLimit.toNumber() * 1.3,
           }),
         callback,
         onError,
